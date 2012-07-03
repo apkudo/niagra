ef7fb2441dd8ed72332c6229f40c70ebb93aa007
Josh Matthews <joshmatthews@gmail.com>
2012-07-03 16:22:49 -0400

    Update example

--- niagrad/example/test.js
+++ niagrad/example/test.js
@@ -1,23 +1,40 @@
 var http = require('http');
 var pid = process.pid
 
-function sigusr1() {
-    console.log('[' + pid + ']', 'Got SIGUSR1')
+var server = http.createServer(helloWorld)
+
+function sigusr2() {
+    console.log('[' + pid + ']', 'Got SIGUSR2, connection count ' + server.connections)
     /* Stop accepting connection on this server, we will eventually
        exit when there is no more work to do */
-    server.watcher.stop()
+    server.close()
+
+    server.on("request", function(req, res) {
+        req.connection.end()
+    })
+}
+
+function server_close() {
+    console.log('[' + pid + ']', 'Closed all connections, exiting')
+    process.exit()
+}
+
+function server_error() {
+    console.log('[' + pid + ']', 'Got error, exiting')
+    process.exit()
 }
 
 function helloWorld(req, res) {
     console.log('[' + pid + ']', req.url, req.socket.fd)
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World I am ' + pid + '\n');
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.end('Hello World, I am ' + pid + '\n')
 }
 
-var server = http.createServer(helloWorld)
 
-process.on('SIGUSR1', sigusr1)
+server.on("close", server_close)
+server.on("error", server_error)
+process.on("SIGUSR2", sigusr2)
 
-server.listenFD(3)
+server.listen({ fd: 3 })
 
 console.log('[' + pid + ']', 'Started')
